4a29e50703a7f91d710649caa9e909b56264786a
t-vinai <86148706+t-vinai@users.noreply.github.com>
2026-02-06 19:47:10 +0700

    add controller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,29 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.entity.UserEntity;
+import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public List<UserEntity> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{userId}")
+    public UserEntity getUserById(@PathVariable Integer userId) {
+        return userRepository.getById(userId);
+    }
 }
